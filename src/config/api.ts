8daa73960f1e52ed86f4b100bd2900baa49c81a3
11kamesh <123620901+elevenkamesh@users.noreply.github.com>
2025-09-17 10:37:20 +0530

    Update api.ts for http to https

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,8 +1,9 @@
 // API Configuration
 export const API_CONFIG = {
-  BASE_URL: 'http://16.171.173.130:3000', // Change this URL as needed
+  // BASE_URL: 'http://16.171.173.130:3000', // Change this URL as needed
+  BASE_URL: '/api', // Change this URL as needed
   ENDPOINTS: {
     CHAT: '/chat',
     HISTORY: '/chat/history',
   }
-};
+};  
